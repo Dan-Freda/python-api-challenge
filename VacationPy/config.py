fc4c7931433afffd41ed119e5f3c5d80a8fc3254
unknown <danielfreda3@gmail.com>
2021-06-02 17:57:48 -0400

    Added observable trends to WeatherPy and heatmap/hotel map markers pngs to Images

--- VacationPy/config.py
+++ VacationPy/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-api_key = "64c66bc5824e5c768069994b6f79c291"
+api_key = "'"
 
 # Google API Key
-g_key = "AIzaSyDwBoovv4YCWr6kuQoKvctvopv1Vv9VCBQ"
+g_key = "'"
--- WeatherPy/config.py
+++ WeatherPy/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-api_key = "64c66bc5824e5c768069994b6f79c291"
+api_key = "'"
 
 # Google API Key
-g_key = "AIzaSyB1Espqf35-KqLRVEjHjIaCsWMt-Gr57lk"
+g_key = "''"
